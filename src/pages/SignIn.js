1bf05da3a3960c6cba9663c662cc99a7b7186e3f
basganajaah <bandyagaadiansyah@gmail.com>
2023-02-10 17:01:15 +0700

    fixed ui login

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,12 @@
-import SigninImage from "../assets/SigninImg.png";
 import React, { useState } from "react";
 import "../styles/SigninStyle.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import APIAuth from "../apis/APIAuth";
 import { Alert } from "react-bootstrap";
-import Logo from "../assets/Logo.png";
-import SigninImg from "../assets/SigninImg.png";
+import LoginLogo from "../assets/Logo.png";
+import LoginImg from "../assets/login.jpg"
+// import SigninImg from "../assets/SigninImg.png";
+// import SigninImage from "../assets/SigninImg.png";
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -61,16 +62,15 @@ const SignIn = () => {
   };
 
   return (
-    <div className="container-signin" data-testid="signin-test">
-      <div className="div-form"></div>
-      <div className="div-text">
-        <div className="signBox">
-          <div className="logo">
-            <img src={Logo} alt="" className="rectangle" />
-          </div>
-          <div className="header">
-            <h1>Welcome, Admin BCR</h1>
-          </div>
+    <div className="flex flex-col lg:flex-row justify-center h-screen w-full">
+      <div className="xl:w-2/3 hidden xl:block">
+        <img className="w-full h-full object-cover" src={LoginImg} alt="LoginImg" />
+      </div>
+
+      <div className="flex flex-col xl:w-1/3 lg:w-full justify-center">
+        <form onSubmit={handleSubmit} className="max-w-[500px] w-full mx-auto lg:p-0 p-8 px-8 rounded-lg">
+          <img src={LoginLogo} alt="LoginLogo" />
+          <h2 className="text-4xl dark:text-black font-bold pt-4 pb-6">Welcome, Admin BCR!</h2>
           <Alert
             style={{ width: "auto", float: "right" }}
             show={alertFail}
@@ -80,22 +80,33 @@ const SignIn = () => {
           >
             <small>Masukkan username dan password yang benar. Perhatikan penggunaan huruf kapital.</small>
           </Alert>
-          <div className="login">
-            <form onSubmit={handleSubmit}>
-              <div className=" input login-email">
-                <label>Email</label>
-                <input className="inpt" type="email" name="email" placeholder="Contoh: johndee@gmail.com" />
-              </div>
-              <div className=" input login-pass">
-                <label>Password</label>
-                <input className="inpt" type="password" name="password" placeholder="6+ karakter" />
-              </div>
-              <button className="sign-in" type="submit">
-                Sign In
-              </button>
-            </form>
+          <div className="flex flex-col text-black-900 py-2">
+            <label className="font-medium text-lg">Email</label>
+            <input
+              className="text-center text-lg rounded-xl border mt-2 p-2 focus:bg-indigo-50"
+              type="email"
+              name="email"
+              placeholder="Contoh: johndee@gmail.com"
+            />
           </div>
-        </div>
+
+          <div className="flex flex-col text-black-900 py-2">
+            <label className="font-medium text-lg">Password</label>
+            <input
+              className="text-center text-lg rounded-xl border mt-2 p-2 focus:bg-indigo-50"
+              type="password"
+              name="password"
+              placeholder="6+ Karakter"
+            />
+          </div>
+
+          <button
+            className="w-full my-5 py-2 rounded-xl bg-blue-700 shadow-xl shadow-blue-200 hover:bg-blue-600 transition duration-300 dark:text-white font-medium text-lg"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </form>
       </div>
     </div>
   );
