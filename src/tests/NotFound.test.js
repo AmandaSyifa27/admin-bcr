81e1722e81855c76d51ed5b024f8068e123e0c82
Amandaruffy <amandasifa011@gmail.com>
2023-02-18 22:36:11 +0700

    add pipelie file

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,16 +1,16 @@
 import { screen } from "@testing-library/react";
 import React from "react";
 import NotFoundPage from "../pages/NotFoundPage";
-import renderWithRouter from "../utils/testUtils";
+import renderWithProvider from "../utils/testUtils";
 
 describe("Not Found page test", () => {
   it("should render link element correctly", () => {
-    renderWithRouter(<NotFoundPage />);
+    renderWithProvider(<NotFoundPage />);
     expect(screen.getByRole(/notfoundpage/i)).toBeInTheDocument();
   });
 
   it("should contain infromation", () => {
-    renderWithRouter(<NotFoundPage />);
+    renderWithProvider(<NotFoundPage />);
     const errorStatus = screen.getByText("404");
     expect(errorStatus).toBeInTheDocument();
 
@@ -19,12 +19,12 @@ describe("Not Found page test", () => {
   });
 
   it("should render link to go back", () => {
-    renderWithRouter(<NotFoundPage />);
+    renderWithProvider(<NotFoundPage />);
     expect(screen.getByText(/back to dashboard/i)).toBeInTheDocument();
   });
 
   it("should have possibility description", () => {
-    renderWithRouter(<NotFoundPage />);
+    renderWithProvider(<NotFoundPage />);
     expect(screen.getByText(/Maybe you entered the wrong word/i)).toBeInTheDocument();
   });
 });
--- src/tests/Signin.test.js
+++ src/tests/Signin.test.js
@@ -1,5 +1,5 @@
 import { cleanup, screen } from "@testing-library/react";
-import renderWithRouter from "../utils/testUtils";
+import renderWithProvider from "../utils/testUtils";
 import SignIn from "../pages/SignIn.js";
 
 afterEach(() => {
@@ -8,7 +8,7 @@ afterEach(() => {
 
 describe("Sign in page test", () => {
   it("should render signin page component", () => {
-    renderWithRouter(<SignIn />);
+    renderWithProvider(<SignIn />);
     const signinElement = screen.getByTestId("signin-test");
     expect(signinElement).toBeInTheDocument();
     expect(signinElement).toHaveTextContent(/welcome, admin bcr/i);
@@ -19,7 +19,7 @@ describe("Sign in page test", () => {
   });
 
   // it("shuold have a form element", () => {
-  //   renderWithRouter(<SignIn />);
+  //   renderWithProvider(<SignIn />);
   //   const form = screen.getByTag("form");
   //   expect(form).toBeInTheDocument();
   // });
